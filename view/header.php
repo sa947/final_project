f1c508f2709a4fc8c9b67c91d0e0d33c36b6b228
sa947 <sa947@njit.edu>
2017-08-04 22:46:33 -0400

    Added header file

---
+++ view/header.php
@@ -0,0 +1,12 @@
+<html>
+   <head>
+         <title>MY TASK MANAGER</title>
+	       <link rel="stylesheet" type="text/css"
+	                href="../style.css">
+			   </head>
+			      <body>
+			            <header>
+				             <h1>MY TASK MANAGER</h1>
+					           </header>
+						      </body>
+						      </html>
