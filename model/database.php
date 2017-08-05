ed1133b9e0d3282b5c154502f3bc1b81746c0a84
sa947 <sa947@njit.edu>
2017-08-04 22:42:17 -0400

    Added database connection file

---
+++ model/database.php
@@ -0,0 +1,40 @@
+<?php
+// database class containing attributes and values used to connect to database
+   class Database {
+          private static $dsn = 'mysql:host=sql2.njit.edu;dbname=sa947';
+	         private static $username = 'sa947';
+		        private static $password = 'YTdrvH89E';
+			       private static $db;
+			          
+				         private function __construct() {}
+					    
+					           public static function getDB
+						   () {
+						              if
+							      (!isset(self::$db))
+							      {
+							                     try
+									     {
+									                        self::$db
+												=
+												new
+												PDO(self::$dsn,
+												                                        self::$username,
+																	                                        self::$password);
+																						               }
+																							       catch
+																							       (PDOException
+																							       $e)
+																							       {
+																							                          $error_message
+																										  =
+																										  $e->getMessage();
+																										                     include('database_error.php');
+																												                        exit();
+																															               }
+																																                  }
+																																		             return
+																																			     self::$db;
+																																			            }
+																																				       }
+																																				          ?>
