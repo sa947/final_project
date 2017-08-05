70a417195a9048feeb8e49b100ea317532dc3152
sa947 <sa947@njit.edu>
2017-08-04 22:47:33 -0400

    Added footer file

---
+++ view/footer.php
@@ -0,0 +1,9 @@
+<html>
+   <body>
+         <footer>
+	          <p class="copyright">
+		              &copy; <?php echo date("Y"); ?> My Task Manager, Inc.
+			               </p>
+				             </footer>
+					        </body>
+						</html>
